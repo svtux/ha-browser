0e29dd608cfdc4b8aaaf58d045902b586f7b084c
Sergey V. Shlepakov <sv.shlepakov@gmail.com>
2017-04-08 16:01:16 +0300

    Процессор спонсоров переделан для сохранения итоговых данных по ставкам.

--- sponsor-processor.php
+++ sponsor-processor.php
@@ -4,19 +4,21 @@
  */
 Header('Access-Control-Allow-Origin: *');
 
-$data = $_POST['data']."\n";
-$world = "3-2";
+$data = $_POST['data'];
+$table = $_POST['table'];
+$dir = $_POST['dir'];
+$timestamp = $_POST['timestamp'];
 
 try {
-	$dbfile = 'db/' . $world . '.txt';
-	$adt = fopen($dbfile, 'x');
-	if ($adt) {
-		fwrite($adt, $data);
-		fclose($adt);
-	} else {
-		file_put_contents($dbfile, $data, FILE_APPEND | LOCK_EX);
-	}
-	echo "1";
-} catch(Exception $e) {
-	echo "0";
+    $dbfile = 'db/' . $table . '/' . $dir . '/' . $timestamp . '.html';
+    $adt = fopen($dbfile, 'x');
+    if ($adt) {
+        fwrite($adt, $data);
+        fclose($adt);
+    } else {
+        file_put_contents($dbfile, $data, FILE_APPEND | LOCK_EX);
+    }
+    echo "1";
+} catch (Exception $e) {
+    echo "0";
 }

7e539920414fd16cde2545fab618cd4cc9b121c6
Sergey V. Shlepakov <sv.shlepakov@gmail.com>
2017-05-01 18:52:02 +0300

    Обновление сохранения данных: - сохранение матча перенесено в скрипт save-data.php; - добавлен параметр matchID для сохранения матча по его ID, а не по дате.

--- save-data.js
+++ save-data.js
@@ -1,10 +1,16 @@
 (function() {
 // сохранение данных матча на сервер
+// javascript:var script = document.createElement("script"); script.src="http://ha-browser.itdom.org/save-data.js"; script.onload=function(){document.body.removeChild(script)}; document.body.appendChild(script);
+	if (!correctPageURL("public_match_info.php&match_id=")) {
+		alert("Скрипт не будет работать на этой странице.\nОткройте страницу с матчем.");
+		return;
+	}
+	var matchId = location.search.match(/match_id=(\d+)/)[1];
 	var body = document.getElementById("page").outerHTML.replace(/<script[^]*?<\/script>/gm,"").replace(/<link[^]*?>/g,"").replace(/color:#ffffff/g,"");
 	var matchDate = body.match(/(\d\d\.\d\d\.\d\d\d\d)/m)[0];
 	var date = matchDate.split(".").join("");
 	var title = body.match(new RegExp("МАТЧ[^]*" + matchDate,"gm"))[0].replace(/<.*?>/gm, "").replace(/\s\s*/gm," ");
-	var url = 'http://ha-browser.itdom.org/save-game.php';
+	var url = 'http://ha-browser.itdom.org/save-data.php';
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -17,7 +23,9 @@
 			alert('OK: ' + xhr.responseText);
 		}
 	};
-	xhr.send(["date=" + date, "title=" + title, "body=" + encodeURIComponent(body)].join("&"));
+	xhr.send(["matchId=" + matchId, "date=" + date, "title=" + title, "body=" + encodeURIComponent(body)].join("&"));
 
-// javascript:var script = document.createElement("script"); script.src="http://ha-browser.itdom.org/save-data.js"; script.onload=function(){document.body.removeChild(script)}; document.body.appendChild(script);
+	function correctPageURL(searchString) {
+		return location.href.indexOf(searchString) !== -1;
+	}
 })();
--- save-data.php
+++ save-data.php
@@ -1,22 +1,50 @@
 <?php
 /**
- * Save Hockey Arent game data processor
+ * Save Hockey Arena game data
  */
 Header('Access-Control-Allow-Origin: *');
 
 $table = $_POST['table'];
-$data = $_POST['data'] . "\n";
+$matchID = $_POST['matchId'];
 
-try {
-	$dbfile = 'db/' . $table . '.txt';
-	$adt = fopen($dbfile, 'x');
-	if ($adt) {
-		fwrite($adt, $data);
-		fclose($adt);
-	} else {
-		file_put_contents($dbfile, $data, FILE_APPEND | LOCK_EX);
-	}
-	echo "1";
-} catch (Exception $e) {
-	echo "0";
+if (isset($table)) {
+    $data = $_POST['data'] . "\n";
+
+    $dbfile = 'db/' . $table . '.txt';
+    saveDataToFile($dbfile, $data);
+} else if (isset($matchID)) {
+    $body = $_POST['body'];
+    $title = $_POST['title'];
+    $matchDate = $_POST['date'];
+    $data = '<div id="matchId_' . $matchID . '">'
+        .'<input type="hidden" class="match-id" value="' . $matchID . '">'
+        .'<input type="hidden" id="innerTitle_' . $matchID . '" value="' . $title .'">'
+        .'<div id="matchContent_' . $matchID . '">' . $body . '</div></div>';
+
+    $fileDir = 'db/games/' . $matchDate;
+
+    try {
+        if (!is_dir($fileDir)) {
+            mkdir($fileDir);
+        }
+        $dbfile = $fileDir . '/' . $matchID . '.html';
+        saveDataToFile($dbfile, $data);
+    } catch (Exception $e) {
+        echo "0";
+    }
+}
+
+function saveDataToFile($filename, $savingData) {
+    try {
+        $adt = fopen($filename, 'x');
+        if ($adt) {
+            fwrite($adt, $savingData);
+            fclose($adt);
+        } else {
+            file_put_contents($filename, $savingData, FILE_APPEND | LOCK_EX);
+        }
+        echo "1";
+    } catch (Exception $e) {
+        echo "0";
+    }
 }
